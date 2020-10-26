ddb29e91fc3eeff7384e41d2409c8ff54e757a34
Jorge-Neto <53627696+Jorge-Neto@users.noreply.github.com>
2020-10-26 12:53:36 -0300

    Facilitanto o entendimento
    Trocando números por variáveis

--- js/comandos.js
+++ js/comandos.js
@@ -1,21 +1,27 @@
 var folhaDesenho = document.getElementById("folha");
 var lapis = folhaDesenho.getContext("2d");
 
+var larguraCampo = 600;
+var alturaCampo = 500;
+var larguraLinha = 5;
+var dimensaoBola = 7;
+var alturaRaquete = 45;
+
 //cor do lápis
 lapis.fillStyle = '#000000';
 //area do desenho (posições)
 //rect = retângulo (X-inicial, Y-inicial, X-final, Y-final)
-lapis.fillRect(0, 0, 600, 500);
+lapis.fillRect(0, 0, larguraCampo, alturaCampo);
 
 //divisória central
 lapis.fillStyle = '#FFFAF0';
-lapis.fillRect(297.5, 0, 5, 500);
+lapis.fillRect(larguraCampo/2 - larguraLinha/2, 0, larguraLinha, alturaCampo);
 
 //raquete do player
-lapis.fillRect(0, 130, 5, 40);
+lapis.fillRect(0, 130, larguraLinha, alturaRaquete);
 
 //raquete do com
-lapis.fillRect(595, 330, 5, 50);
+lapis.fillRect(larguraCampo - larguraLinha, 330, larguraLinha, alturaRaquete);
 
 //bola
-lapis.fillRect(200, 150, 7, 7);
+lapis.fillRect(200, 150, dimensaoBola, dimensaoBola);
